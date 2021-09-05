b817990928684ae059e9a699b07ac6bcdceb4b70
DanielaVL <daniela.vasquezl1@udea.edu.co>
2021-09-05 17:34:14 -0500

    Últimos detalles en los componentes para hacer la entrega del primer módulo

--- Aplicacion-Angular/src/app/listador/listador.component.ts
+++ Aplicacion-Angular/src/app/listador/listador.component.ts
@@ -6,23 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listador.component.css']
 })
 export class ListadorComponent implements OnInit {
-  pais: string[]= [
+  paises: string[]= [
     "Grecia",
     "Francia",
     "España",
     "México",
   ];
 
-  url: string[]= [
+  urls: string[]= [
     "https://www.google.com/search?q=grecia&rlz=1C1LENN_enCO488CO488&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjQiIrutefyAhWlRzABHYStAm4Q_AUoAnoECAEQBA&biw=1366&bih=657#imgrc=vCU5BY4O2cbT6M&imgdii=eoGy1gahDU7CuM",
     "https://www.google.com/search?q=francia&rlz=1C1LENN_enCO488CO488&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjGiZaEtufyAhW3SjABHQXLAxQQ_AUoAnoECAEQBA&biw=1366&bih=657#imgrc=ZBqa7cksQiJ0-M",
     "https://www.google.com/search?q=espa%C3%B1a&rlz=1C1LENN_enCO488CO488&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjH8IyVtufyAhWORDABHfIAA8IQ_AUoAnoECAEQBA&biw=1366&bih=657#imgrc=NUUxsH8txvK8KM",
     "https://www.google.com/search?q=mexico+el+angel&tbm=isch&ved=2ahUKEwjSmuygtufyAhUliOAKHbkLDPEQ2-cCegQIABAA&oq=mexico+el+angel&gs_lcp=CgNpbWcQAzIFCAAQgAQyBQgAEIAEMgYIABAFEB4yBggAEAgQHjoHCAAQsQMQQzoECAAQQ1DVgwFYj4wBYJSOAWgAcAB4AIABigKIAc8KkgEFMC44LjGYAQCgAQGqAQtnd3Mtd2l6LWltZ8ABAQ&sclient=img&ei=6oA0YdL9IaWQgge5l7CIDw&bih=657&biw=1366&rlz=1C1LENN_enCO488CO488#imgrc=O4Gu41hT7hYiJM&imgdii=WmRYXz7vik-3mM",
   ];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+
+    /*Esto es una plantilla de lo que pasaría al dar click en el botón "Aceptar" pero no lo sé usar,
+      quería usar una función llamada "paísEnLaLista" y claramente no usaría valores quemados 
+      sino los input "destino" y "url" 
+    */
+    
+    console.log(this.paises)
+    const newCountry='Brasil';
+    const newUrl='https://www.google.com/search?q=brasil&rlz=1C1LENN_enCO488CO488&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjJk63p6OjyAhV-SjABHXKKDCwQ_AUoAXoECAEQAw&biw=1366&bih=657#imgrc=TQSWXHq2Wnn6lM&imgdii=niKtqQISkAWgnM'
+    console.log(newCountry)
+    if(!this.paises.includes(newCountry)){
+      this.paises.push(newCountry)
+      this.urls.push(newUrl)
+    }
+    console.log(this.paises)
   }
 
 }
